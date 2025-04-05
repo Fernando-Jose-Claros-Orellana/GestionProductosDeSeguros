0512f676b32f3768518417847d84a5f6e9ec62d7
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2025-04-04 23:26:52 -0600

    Se actualizan DTOs

--- GestionProductosDeSeguros.DTOs/ProductoDTOs/ActualizaProductoDTO.cs
+++ GestionProductosDeSeguros.DTOs/ProductoDTOs/ActualizaProductoDTO.cs
@@ -1,19 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GestionProductosDeSeguros.DTOs.ProductoDTOs
 {
-    class ActualizaProductoDTO
+   public class ActualizaProductoDTO
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El Nombre es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El Nombre no puede exceder los 100 caracteres.")]
         public string Nombre { get; set; }
+
         public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El Tipo de Producto es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El Tipo de Producto no puede exceder los 100 caracteres.")]
         public string TipoProductoNombre { get; set; }
+
+        [Required(ErrorMessage = "La Forma de Pago es obligatoria.")]
+        [StringLength(50, ErrorMessage = "La Forma de Pago no puede exceder los 50 caracteres.")]
         public string FormaPagoNombre { get; set; }
+
+        [Required(ErrorMessage = "La Prima Anual es obligatoria.")]
+        [DataType(DataType.Currency)]
         public decimal PrimaAnual { get; set; }
+
+        [Required(ErrorMessage = "La Suma Asegurada es obligatoria.")]
+        [DataType(DataType.Currency)]
         public decimal SumaAsegurada { get; set; }
 
     }
--- GestionProductosDeSeguros.DTOs/ProductoDTOs/ObtenerProductosDTO.cs
+++ GestionProductosDeSeguros.DTOs/ProductoDTOs/ObtenerProductosDTO.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GestionProductosDeSeguros.DTOs.ProductoDTOs
 {
-  public  class BuscarProductosDTO
+   public class ObtenerProductosDTO
     {
-        public string NombreBusqueda { get; set; }
-
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
