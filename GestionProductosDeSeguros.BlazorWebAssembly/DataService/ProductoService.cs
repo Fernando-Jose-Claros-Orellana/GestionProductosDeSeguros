976d1f7ec7f843d1011bed9241278d79a52951d5
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2025-04-05 00:46:54 -0600

    Se configura par recibir la api y hacer peticiones atraves de un servicio

---
+++ GestionProductosDeSeguros.BlazorWebAssembly/DataService/ProductoService.cs
@@ -0,0 +1,65 @@
+﻿using GestionProductosDeSeguros.DTOs.ProductoDTOs;
+using System.Net.Http.Json;
+
+namespace GestionProductosDeSeguros.BlazorWebAssembly.DataService
+{
+    public class ProductoService
+    {
+        private readonly HttpClient _httpClient;
+        public ProductoService(IHttpClientFactory httpCli)
+        {
+            _httpClient = httpCli.CreateClient("API");
+        }
+
+        public async Task<List<ObtenerProductosDTO>> ObtenerProductos()
+        {
+            var response = await _httpClient.GetAsync("Producto");
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<List<ObtenerProductosDTO>>();
+                return result ?? new List<ObtenerProductosDTO>();
+            }
+            return new List<ObtenerProductosDTO>();
+        }
+
+        public async Task<List<BuscarProductosDTO>> BuscarPorNombre(string nombre)
+        {
+            var response = await _httpClient.GetAsync($"Producto/buscar");
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<List<BuscarProductosDTO>>();
+                return result ?? new List<BuscarProductosDTO>();
+            }
+            return new List<BuscarProductosDTO>();
+        }
+
+        public async Task<ActualizaProductoDTO> ObtenerProductoId(int id)
+        {
+            var response = await _httpClient.GetAsync($"Producto/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<ActualizaProductoDTO>();
+                return result ?? new ActualizaProductoDTO();
+            }
+            return new ActualizaProductoDTO();
+        }
+
+        public async Task<bool> CrearProducto(CrearProductoDTO producto)
+        {
+            var response = await _httpClient.PostAsJsonAsync("Producto", producto);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> ActualizarProducto(ActualizaProductoDTO producto)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"Producto/{producto.Id}", producto);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> EliminarProducto(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"Producto/{id}");
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
--- GestionProductosDeSeguros.BlazorWebAssembly/Program.cs
+++ GestionProductosDeSeguros.BlazorWebAssembly/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using GestionProductosDeSeguros.BlazorWebAssembly;
+using GestionProductosDeSeguros.BlazorWebAssembly.DataService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<ProductoService>();
+
+builder.Services.AddHttpClient("API", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7057/api/");
+});
 
 await builder.Build().RunAsync();
